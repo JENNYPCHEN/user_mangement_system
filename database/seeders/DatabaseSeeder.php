71cd9e876d91d6562d73ffc7b1235e93a2a9b83d
Jenny Chen <pelgrims.chenchingyi@gmail.com>
2022-05-16 16:53:05 +0200

    homepage, local language, timezone

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@hello.com',
+            'password'=>Hash::make('password'),
+            'is_admin'=>'1'
+        ]);
+        User::create([
+            'name'=>'user1',
+            'email'=>'user1@hello.com',
+            'password'=>Hash::make('password'),
+        ]);
+        User::create([
+            'name'=>'user2',
+            'email'=>'user2@hello.com',
+            'password'=>Hash::make('password'),
+        ]);
     }
 }
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('title', 'Accueil')
+@section('section')
+<section="homepage">
+    <div class="d-flex justify-content-center align-items-center my-5 py-5">
+        <img class="img-thumbnail img-fluid rounded" width="200" height="auto" src={{asset('images/logo.png')}} />
+    </div>
+
+
+</section>
+@endsection
